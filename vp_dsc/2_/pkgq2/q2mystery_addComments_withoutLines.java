3f4ece1d944eda97b09bb3add5234c2b2cc0afd3
vins <vinsep@berkeley.edu>
2026-02-23 14:30:36 -0800

    discussion pkgs continued. also weary of overengineering/overthinking. small # or low complexity mods can go far!

---
+++ vp_dsc/2_/pkgq2/q2mystery_addComments_withoutLines.java
@@ -0,0 +1,20 @@
+public static int mystery(int[] inputArray, int k) {
+    int x = inputArray[k];
+    int answer = k;
+    int index = k + 1;
+    while (index < inputArray.length) {
+        if (inputArray[index] < x) {
+            x = inputArray[index];
+            answer = index;
+        }
+        index = index + 1;
+    }
+    return answer;
+}
+
+/*
+This is a function (a.k.a. method). It takes an array of integers and an integer as
+arguments, and returns an integer.
+(a) Describe what mystery returns if inputArray = [3, 0, 4, 6, 3] and k = 2.
+(b) Can you explain in plain English what mystery does?
+*/
