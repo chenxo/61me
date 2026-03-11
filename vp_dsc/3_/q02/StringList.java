ad9549f145a228822008dd422f7beda4c2c5faa1
chenxo <vinsep@berkeley.edu>
2026-03-11 11:49:17 -0700

    dsc3. q01, q02 done. mve on to proj0, lab3, prj1, dsc4.

---
+++ vp_dsc/3_/q02/StringList.java
@@ -0,0 +1,26 @@
+public class StringList {
+    String str;
+    StringList rest;
+    public StringList(String str, StringList rest) {
+        this.str = str;
+        this.rest = rest;
+    }
+    /**
+     *2 To Do List
+    Draw the box-and-pointer diagram that results from running the following code. A StringList is
+    similar to an IntList. It has two instance variables, first and rest.
+     */
+    public static void main(String[] args) {
+        StringList L = new StringList("eat", null);
+        L = new StringList("should", L);
+        L = new StringList("you", L);
+        L = new StringList("sometimes", L);
+        StringList M = L.rest;
+        StringList R = new StringList("many", null);
+        R = new StringList("potatoes", R);
+        R.rest.rest = R;
+        M.rest.rest.rest = R.rest;
+        L.rest.rest = L.rest.rest.rest;
+        L = M.rest;
+    }
+}
